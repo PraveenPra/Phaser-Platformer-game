950c4db04dc6e30c8fbc1fc12f077a5f7881a2bc
Manikanta <prvp6501@gmail.com>
2026-01-01 11:20:07 +0530

    Enemy ground state used

--- src/v2/entities/enemy/Enemy.js
+++ src/v2/entities/enemy/Enemy.js
@@ -1,12 +1,12 @@
 import { Character } from "../common/Character.js";
 import { resolveProfile } from "../digimon/resolveProfile.js";
-import { GroundStates } from "../common/states/groundStates.js";
+import { EnemyGroundStates } from "../common/states/EnemyGroundStates.js";
 import { EnemyAI } from "./EnemyAI.js";
 
 export class Enemy extends Character {
   constructor(scene, x, y, textureKey) {
     const profile = resolveProfile(textureKey);
-    super(scene, x, y, textureKey, profile, GroundStates, "idle");
+    super(scene, x, y, textureKey, profile, EnemyGroundStates, "idle");
 
     this.ai = new EnemyAI();
   }
